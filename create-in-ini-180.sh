c058784b21ad5b8f5736b67496145937accbb4fd
Danny Ramirez <38297146+dannyr6rh@users.noreply.github.com>
2025-04-13 13:17:00 -0600

    Update create-in-ini-180.sh

--- create-in-ini-180.sh
+++ create-in-ini-180.sh
@@ -1,7 +1,7 @@
 for ip in $(seq 1 254); do
-  echo "180.160.1.$ip" >> inventario_tmp.txt
+  echo "180.160.1.$ip" >> inventario_tmp_180.txt
 done
 
-echo "[segmento]" | cat - inventario_tmp.txt > inventario_final180.ini
+echo "[segmento]" | cat - inventario_tmp_180.txt > inventario_final180.ini
 
-ansible -i inventario_fina180l.ini segmento -m ping > resultado.txt
+ansible -i inventario_fina180l.ini segmento -m ping > resultado_180.txt
