60d0a88df879480b22560ea5dbf38b0f1c8b269d
Danny Ramirez <38297146+dannyr6rh@users.noreply.github.com>
2025-04-13 13:12:16 -0600

    Create create-in-ini-192.sh

---
+++ create-in-ini-192.sh
@@ -0,0 +1,7 @@
+for ip in $(seq 1 254); do
+  echo "192.168.1.$ip" >> inventario_tmp.txt
+done
+
+echo "[segmento]" | cat - inventario_tmp.txt > inventario_final192.ini
+
+ansible -i inventario_final.ini segmento -m ping > resultado.txt
