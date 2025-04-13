9ad72d841b11fe4a2a51ba2a6b5493903c237fc3
Danny Ramirez <38297146+dannyr6rh@users.noreply.github.com>
2025-04-13 13:16:28 -0600

    Update create-in-ini-192.sh

--- create-in-ini-192.sh
+++ create-in-ini-192.sh
@@ -1,7 +1,7 @@
 for ip in $(seq 1 254); do
-  echo "192.168.1.$ip" >> inventario_tmp.txt
+  echo "192.168.1.$ip" >> inventario_tmp_192.txt
 done
 
-echo "[segmento]" | cat - inventario_tmp.txt > inventario_final192.ini
+echo "[segmento]" | cat - inventario_tmp_192.txt > inventario_final192.ini
 
-ansible -i inventario_final192.ini segmento -m ping > resultado.txt
+ansible -i inventario_final192.ini segmento -m ping > resultado_192.txt
